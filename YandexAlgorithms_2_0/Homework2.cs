05052dfbe46db60f7a9bed2c7190059ac36e7db5
Vlad Korenev <korenevlad17@gmail.com>
2023-10-12 18:45:47 +0300

    Homework2/release

---
+++ YandexAlgorithms_2_0/Homework2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YandexAlgorithms_2_0
+{
+    internal class Homework2
+    {
+    }
+}
