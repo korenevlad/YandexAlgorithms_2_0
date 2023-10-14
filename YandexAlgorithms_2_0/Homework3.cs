b33c97e2ab98864de6f15dbd3842ea819c0da7a9
Vlad Korenev <korenevlad17@gmail.com>
2023-10-14 20:26:25 +0300

    Homework3/commit2

--- YandexAlgorithms_2_0/Homework3.cs
+++ YandexAlgorithms_2_0/Homework3.cs
@@ -9,30 +9,54 @@ namespace YandexAlgorithms_2_0
     public class Homework3
     {
 
-        public static void A_Solution(string firstStr, string secondStr)
-        {
-            string[] firstMasStr = firstStr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            string[] secodMasStr = secondStr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        //public static void A_Solution(string firstStr, string secondStr)
+        //{
+        //    string[] firstMasStr = firstStr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        //    string[] secodMasStr = secondStr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-            HashSet<string> hashStr = new HashSet<string>();
-            for (int i = 0; i < firstMasStr.Length; i++)
+        //    HashSet<string> hashStr = new HashSet<string>();
+        //    for (int i = 0; i < firstMasStr.Length; i++)
+        //    {
+        //        hashStr.Add(firstMasStr[i]);
+        //    }
+        //    int count = 0;
+        //    for (int i = 0; i < secodMasStr.Length; i++)
+        //    {
+        //        if (hashStr.Contains(secodMasStr[i]))
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    Console.WriteLine(count);
+        //}
+
+        public static void B_Solution(string str)
+        {
+            string[] strMas = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            HashSet<string> strHash = new HashSet<string>();
+            for (int i =0; i < strMas.Length; i++)
             {
-                hashStr.Add(firstMasStr[i]);
-            }
-            int count = 0;
-            for (int i = 0; i < secodMasStr.Length; i++)
-            {
-                if (hashStr.Contains(secodMasStr[i]))
+                if (strHash.Contains(strMas[i]))
                 {
-                    count++;
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                    strHash.Add(strMas[i]);
                 }
             }
-            Console.WriteLine(count);
         }
 
         public static void Main(string[] args)
         {
-            A_Solution(Console.ReadLine(), Console.ReadLine());
+            //Задача А
+            //
+            //A_Solution(Console.ReadLine(), Console.ReadLine());
+
+            //Задача B
+            //
+            B_Solution(Console.ReadLine());
         }
     }
 }
