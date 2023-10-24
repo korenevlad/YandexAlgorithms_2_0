a85ca682852bdad3acd45b55927990a6f0485869
Vlad Korenev <korenevlad17@gmail.com>
2023-10-24 16:30:44 +0300

    Homework5/commit3

--- YandexAlgorithms_2_0/PrefixSumsAndTwoPointers_Hw5.cs
+++ YandexAlgorithms_2_0/PrefixSumsAndTwoPointers_Hw5.cs
@@ -83,12 +83,88 @@ namespace YandexAlgorithms_2_0
         }
 
 
+        public static void LeetCode1732(int[] num)
+        {
+            BigInteger[] prefix = new BigInteger[num.Length + 1];
+            prefix[0] = 0;
+            for(int i = 1; i < num.Length + 1; i++)
+            {
+                prefix[i] = prefix[i - 1] + num[i - 1]; 
+            }
+            Console.WriteLine(prefix.Max());
+        }
+
+        public static int LeetCode724(int[] nums)
+        {
+            int result = -1;
+            BigInteger[] prefixSum = new BigInteger[nums.Length + 1];
+            prefixSum[0] = 0;
+            for (int i = 1; i < nums.Length + 1; i ++)
+            {
+                prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+            }
+            for (int i = prefixSum.Length -1; i > 0; i--)
+            {
+                if (prefixSum[prefixSum.Length-1]-prefixSum[i] == prefixSum[i - 1])
+                {
+                    result = i - 1;
+                }
+            }
+            return result;
+        }
+        public static int LetCode560(int[] nums, int k)
+        {
+            int result = 0;
+            int[] prefixSum = new int[nums.Length + 1];
+            prefixSum[0] = 0;
+            for(int i = 1; i < nums.Length + 1; i++)
+            {
+                prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+            }
+            int backPointer = 0;
+            int forwardPointer = 1;
+            while (backPointer < prefixSum.Length-1)
+            {
+                if (forwardPointer < prefixSum.Length)
+                {
+                    if (prefixSum[forwardPointer] - prefixSum[backPointer] == k)
+                    {
+                        result++;
+                        forwardPointer++;
+                    }
+                    else
+                    {
+                        forwardPointer++;
+                    }
+                }
+                else
+                {
+                    backPointer++;
+                    forwardPointer = backPointer + 1;
+                }
+
+            }
+            return result;
+        }
+
+
+
 
         public static void Main(string[] args)
         {
             //A_Solution();
-            
-            B_Solution(Console.ReadLine(), Console.ReadLine());
+
+            //B_Solution(Console.ReadLine(), Console.ReadLine());
+
+
+            //int[] num1 = { -5, 1, 5, 0, -7 };
+            //LeetCode1732(num1);
+
+            //int[] num2 = { 1, 2, 3 };
+            //Console.WriteLine(LeetCode724(num2));
+
+            int[] num3 = { 1, 2, 3 };
+            Console.WriteLine(LetCode560(num3, 3));
         }
     }
 }
